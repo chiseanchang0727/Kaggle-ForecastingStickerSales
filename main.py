f6ae58c1d3c0ad7b098cbac77a85c0ebabbf25bc
chiseanchang0727 <luciuskyear0812@gmail.com>
2025-01-23 12:54:16 +0800

    wip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import joblib
 from src.preprocessing import create_time_features, imputation, encoding, split_and_standardization, target_transformation, reverse_target_transformation
 from src.feature_engineering import generate_lag_features_by_group
 from src.pred_and_eval import Predictor
@@ -8,13 +9,13 @@ def get_data():
 
     train_data = pd.read_csv('./data/train.csv')
     test_data = pd.read_csv('./data/test.csv')
-
+    
     data = pd.concat([train_data, test_data], axis=0).set_index('id')
-    train_data = train_data.set_index('id')
-    test_data = test_data.set_index('id')
-
     train_idx = data.iloc[:train_data.shape[0]].index
     test_idx = data.iloc[train_data.shape[0]:].index
+    
+    train_data = train_data.set_index('id')
+    test_data = test_data.set_index('id')
 
     return train_data, test_data
 
@@ -31,6 +32,15 @@ def data_transformation(df, group_cols, mode):
 
     return df
 
+def save_model(model, file_path='stacking_model.pkl'):
+    joblib.dump(model, file_path)
+    print(f"Model saved to {file_path}")
+    
+def load_model(file_path='stacking_model.pkl'):
+    model = joblib.load(file_path)
+    print(f"Model loaded from {file_path}")
+    return model
+
 def main():
 
     df_train, df_test = get_data()
@@ -44,26 +54,30 @@ def main():
 
     # df_train = df[df.index.isin(train_idx)]
     # df_test = df[df.index.isin(test_idx)].drop(['num_sold'], axis=1)
-    scaler_train, X_train, X_valid, scaler_target, y_train, y_valid = split_and_standardization(df_train, target_col='num_sold', test_size=0.2)
+    _, X_train, X_valid, y_train, y_valid = split_and_standardization(df_train, target_col='num_sold', test_size=0.2)
 
     predictor = Predictor(X_train, X_valid, y_train, y_valid)
     
     mape = predictor.train_and_eval()
-    # mse, y_pred = prediction(X_train_scaled, X_valid_scaled, y_train_scaled, y_valid_scaled)
 
     print(f"MAPE: {mape: .5f}")
+    
+    save_model(predictor.model, 'stacking_model.pkl')
 
-    df_test_scaled = scaler_train.transform(df_test)    
+    # df_test_scaled = scaler_train.transform(df_test)    
 
-    y_test_pred = predictor.predict_on_test(df_test_scaled)
+    # y_test_pred = predictor.predict_on_test(df_test)
+    
+    loaded_model = load_model('stacking_model.pkl')
 
-    # y_test_pred_revert = np.floor(scaler_target.inverse_transform(y_test_pred.reshape(-1, 1)))
+    # Make predictions
+    predictions = loaded_model.predict(df_test)
 
-    y_test_pred_revesed = reverse_target_transformation(y_test_pred)
+    predictions_revesed = reverse_target_transformation(predictions)
     
     sumbmission = pd.DataFrame({
         'id': df_test.index,
-        'num_sold': y_test_pred_revesed.flatten()
+        'num_sold': predictions_revesed
     })
 
     sumbmission.to_csv('submission.csv', index=False)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,11 @@
 import re
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 import holidays
 
-def create_sinusoidal_transformation_year_month_day(df, col_name, year, month, day, period):
+def create_sinusoidal_transformation_year_month_day(df):
 
     # df[f'{col_name}_sin'] = np.sin(2 * np.pi * df[year] * df[month] * df[day] / period)
     # df[f'{col_name}_cos'] = np.cos(2 * np.pi * df[year] * df[month] * df[day] / period)
@@ -34,7 +34,7 @@ def create_time_features(df: pd.DataFrame, date_col='date'):
     #     holiday_cal = holidays.CountryHoliday(country=country)
     #     df[f'{country}_holiday'] = df[date_col].apply(lambda x: x in holiday_cal).astype(int)
 
-    df = create_sinusoidal_transformation_year_month_day(df, 'date', "year", "month", "day", 12)
+    df = create_sinusoidal_transformation_year_month_day(df)
     
     df['group'] = (df['year'] - 2010) * 48 + df['month'] * 4 + df['day'] // 7
 
@@ -46,15 +46,13 @@ def imputation(df: pd.DataFrame, group_by: list):
 
     # df.loc[df.index.isin(train_idx), 'num_sold'] = df.loc[df.index.isin(train_idx), 'num_sold'].fillna(0)
 
-    # df['num_sold'] = df['num_sold'].fillna(df['num_sold'].mean())
+    df['num_sold'] = df['num_sold'].fillna(df['num_sold'].mean())
     
-    df = df.dropna()
+    # df = df.dropna()
 
     # df_temp = df.groupby(group_by)['num_sold'].mean().reset_index(name='avg_sold').round(0)
     # df_merge = pd.merge(df, df_temp, how='left', on=group_by)
     # df_merge['num_sold'] = np.where(df_merge['num_sold'].isna(), df_merge['avg_sold'], df_merge['num_sold'])
-
-
 
     return df
 
@@ -71,9 +69,13 @@ def reverse_target_transformation(input):
 def encoding(df: pd.DataFrame):
 
     categorical_col = df.select_dtypes('object').columns.to_list()
-    df_encoded = pd.get_dummies(df, columns=categorical_col, drop_first=False, dtype=int)
+    # df_encoded = pd.get_dummies(df, columns=categorical_col, drop_first=False, dtype=int)
 
-    return df_encoded
+    for col in categorical_col:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+
+    return df
 
 
 def data_splitting(df, target_col, test_size=0.2):
@@ -93,9 +95,6 @@ def split_and_standardization(df, target_col, test_size):
     X_train_scaled = scaler_train.fit_transform(X_train)
     X_valid_scaled = scaler_train.transform(X_valid)
 
-    scaler_target = StandardScaler()
-    # y_train_scaled = scaler_target.fit_transform(pd.DataFrame(y_train))
-    # y_valid_scaled = scaler_target.transform(pd.DataFrame(y_valid))
 
-    return scaler_train, X_train, X_valid, scaler_target, y_train, y_valid
+    return scaler_train, X_train, X_valid, y_train, y_valid
 
